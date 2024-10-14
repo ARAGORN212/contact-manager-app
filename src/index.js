04cf392929eb639bac9b662e1fd869434472414d
Morteza <33259738+ARAGORN212@users.noreply.github.com>
2024-10-14 21:08:33 +0330

    add react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,7 +14,9 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
